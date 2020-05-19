221115fae1ecc7570a11ed38676768b117ae6847
atte <p2946@student.jamk.fi>
2020-05-19 19:04:02 +0300

    tehtävä 2 tehty

---
+++ T2-Mäkihyppy/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Linq;
+
+namespace T2_Mäkihyppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kokonaispisteet = 0;
+            int kaikkipisteet = 0;
+            int[] pisteet = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Tuomari " + (i + 1)  + " anna tyylipisteet:");
+                    string strpisteet = Console.ReadLine();
+                    if (Int32.TryParse(strpisteet, out pisteet[i]))
+                    {
+                        kaikkipisteet = kaikkipisteet + pisteet[i];
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Eipä ollu numero\n");
+                        continue;
+                    }
+                }
+            }
+
+            kokonaispisteet = kaikkipisteet - pisteet.Max() - pisteet.Min();
+            Console.WriteLine("kaikkien tuomarien pisteet: " + kaikkipisteet);
+            Console.WriteLine("lopulliset pisteet: " + kokonaispisteet);
+        }
+    }
+}
+
