e5a31ba782a1bfde5aca98989e5e91debf3af6ca
atte <p2946@student.jamk.fi>
2020-05-22 20:30:29 +0300

    T3 tehty

---
+++ T3-Kulutus/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace T3_Kulutus
+{
+    class Program
+    {
+
+        public const float kulutus = 7.5f;
+        public const float bensanHinta = 1.55f;
+        static void Main(string[] args)
+        {
+            float matka = 0;
+            float hinta = 0;
+            float määrä = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Anna matka kilometreinä");
+                string strmatka = Console.ReadLine();
+                if (Single.TryParse(strmatka, out matka))
+                {
+                    KulutusLaskuri(matka, ref hinta, ref määrä);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ei onnistu!\n");
+                    continue;
+                }
+            }
+
+            hinta = (float)Math.Round(hinta, 2);
+            määrä = (float)Math.Round(määrä, 2);
+            Console.WriteLine("bensaa kuluu {0} litraa ja se maksaa {1} euroa", määrä, hinta);
+        }
+
+        public static void KulutusLaskuri(float matka, ref float hinta, ref float käytettyBensa)
+        {
+            hinta = (kulutus * bensanHinta * matka) / 100;
+            käytettyBensa = (kulutus * matka) / 100;
+        }
+    }
+}
