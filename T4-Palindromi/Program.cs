b4ba1e6357056140f099169fc3b88e85d532dfe8
atte <p2946@student.jamk.fi>
2020-05-23 11:29:02 +0300

    Teht 4 valmis

---
+++ T4-Palindromi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace T4_Palindromi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna merkkijono");
+            string merkkijono = Console.ReadLine();
+            string merkkijonoLowerCase = merkkijono.ToLower();
+            char[] testattava = merkkijonoLowerCase.ToCharArray();
+            char[] käännetty = stringKääntäjä(merkkijonoLowerCase);
+
+            if (käännetty.SequenceEqual(testattava))
+            {
+                Console.WriteLine(merkkijono + " on palindromi");
+            }
+            else
+            {
+                Console.WriteLine(merkkijono + " ei ole palindromi, sana väärinpäin on " + new string(käännetty));
+            }
+        }
+        static char[] stringKääntäjä(string merkkijono)
+        {
+            char[] käännettävä = merkkijono.ToCharArray();
+            Array.Reverse(käännettävä);
+            return käännettävä;
+        }
+    }
+
+}
+
+
