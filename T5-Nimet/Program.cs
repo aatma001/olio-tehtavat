e1e73932d9bb6e05632c2634367ed643ae84eded
atte <p2946@student.jamk.fi>
2020-05-26 19:42:38 +0300

    5 tehty

---
+++ T5-Nimet/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5_Nimet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            List<string> nimiLista = new List<string>();
+            int monta = 0;
+            Console.WriteLine("Anna henkilöiden nimet. Tyhjä syöte lopettaa.");
+            while (true)
+            {
+                Console.WriteLine("Anna nimi: ");
+                string nimi = Console.ReadLine();
+                if (nimi != "")
+                {
+                    nimiLista.Add(nimi);
+
+                }
+                else
+                {
+                    break;
+                }
+                monta = monta + 1;
+            }
+            nimiLista.Sort();
+            Console.WriteLine(monta + " nimeä annettiin.");
+            foreach (string nimi in nimiLista)
+            {
+                Console.WriteLine(nimi);
+            }
+            
+        }
+    }
+}
