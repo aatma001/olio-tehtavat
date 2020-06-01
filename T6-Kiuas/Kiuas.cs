31003bc4480fd96926bf63100609a8ae6721c138
atte <p2946@student.jamk.fi>
2020-06-01 19:20:32 +0300

    t6 tehty

---
+++ T6-Kiuas/Kiuas.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6_Kiuas
+{
+    class Kiuas
+    {
+        const int maxTemperature = 120;
+        const int maxMoisture = 100;
+        public bool isOn;
+        private int temperature;
+        private int moisture;
+        private int newTemperature;
+        private int newMoisture;
+        public Kiuas(int _temperature, int _moisture)
+        {
+            temperature = _temperature;
+            moisture = _moisture;
+        }
+         public void TurnOn()
+        {
+            isOn = true;
+            Console.WriteLine("Sauna is on.");
+
+        }
+        public void TurnOff()
+        {
+            Console.WriteLine("Sauna is off");
+            isOn = false;
+        }
+
+        public void ChangeTemperature()
+        {
+            while (true)
+            {
+                Console.WriteLine("Give new temperature");
+                newTemperature = int.Parse(Console.ReadLine());
+                if (newTemperature <= maxTemperature)
+                {
+                    Console.WriteLine("Temperature changed.\n");
+                    temperature = newTemperature;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Give value between 0-120");
+                    continue;
+                }
+            }
+        }
+        public void ChangeMoisture()
+        {
+            while (true)
+            {
+                Console.WriteLine("Give new moisture");
+                newMoisture = int.Parse(Console.ReadLine());
+                if (newMoisture <= maxMoisture)
+                {
+                    Console.WriteLine("Moisture changed\n");
+                    moisture = newMoisture;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("give value between 0-100");
+                    continue;
+                }
+            }
+        }
+        public void CheckStatus()
+        {
+            Console.WriteLine("Temperature: {0} Moisture: {1}", temperature, moisture);
+        }
+    }
+}
+
---
+++ T6-Kiuas/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace T6_Kiuas
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool on = true;
+            Console.WriteLine("Set temperature of the sauna");
+            int temperature = int.Parse(Console.ReadLine());
+            Console.WriteLine("Set moisture of the sauna");
+            int moisture = int.Parse(Console.ReadLine());
+
+            Kiuas kiuas = new Kiuas(temperature, moisture);
+            kiuas.TurnOn();
+            kiuas.CheckStatus();
+
+
+            while (on)
+            {
+                Console.WriteLine("Adjust temperature with 1, moisture with 2, and turn the sauna off with 3");
+                int action = int.Parse(Console.ReadLine());
+                switch (action)
+                {
+                    case 1:
+                        kiuas.ChangeTemperature();
+                        kiuas.CheckStatus();
+                        break;
+
+                    case 2:
+                        kiuas.ChangeMoisture();
+                        kiuas.CheckStatus();
+                        break;
+
+                    case 3:
+                        kiuas.TurnOff();
+                        on = false;
+                        break;
+                } 
+            }
+        }   
+    }
+}
