239d7a9dc7e859964e3eaf3ca124b327fdfdbd7c
atte <p2946@student.jamk.fi>
2020-06-06 14:46:53 +0300

    T9 tehty

---
+++ T9-Vehicle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T9_Vehicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle moped = new Vehicle();
+            moped.Brand = "Suzuki";
+            moped.Model = "PV";
+            moped.Tyres = 2;
+            moped.Speed = 45;
+
+            Vehicle car = new Vehicle();
+            car.Brand = "Honda";
+            car.Model = "Civic";
+            car.Tyres = 4;
+            car.Speed = 180;
+
+            Console.WriteLine("Moped:\n " + moped.ShowInfo());
+            Console.WriteLine("Car:\n " + car.ShowInfo());
+            Console.WriteLine("");
+            Console.WriteLine("Moped:\n" + moped.ToString());
+            Console.WriteLine("Car:\n " + car.ToString());
+        }
+    }
+}
---
+++ T9-Vehicle/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T9_Vehicle
+{
+    class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        public Vehicle()
+        {
+
+        }
+
+
+        public string ShowInfo()
+        {
+            return "Brand: " + Brand + " Model: " + Model;
+        }
+
+        public override string ToString()
+        {
+            return "Brand: " + Brand + " Model: " + Model + " Speed: " + Speed + " Tryres: " + Tyres;
+        }
+    }   
+
+}
