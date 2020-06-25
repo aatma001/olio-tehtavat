035d196e9995660e7db50f0e95a671b6dca316d3
atte <p2946@student.jamk.fi>
2020-06-25 16:00:09 +0300

    24 tehty

---
+++ T24-Kortit/CardDeck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace T24_Kortit
+{
+    class CardDeck
+    {
+        public List<Card> Cards { get; set; }
+
+        public CardDeck(List<Card> cards)
+        {
+            Cards = cards;
+        }
+
+
+        public void Shuffle()
+        {
+            Cards = Cards.OrderBy(i => Guid.NewGuid()).ToList();
+        }
+
+    }
+}
---
+++ T24-Kortit/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace T24_Kortit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] cards = { "Ace", "One", "Two", "Three", "Four", "Five", "Six",
+                                "Seven", "Eight", "Nine", "Ten", 
+                                "Jack", "Queen", "King"};
+
+            string[] suits = { "spades", "hearts", "diamonds", "clubs" };
+            List<Card> cardlist = new List<Card>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 13; j++) 
+                {
+                    cardlist.Add(new Card(cards[j], suits[i]));
+                }
+            }
+            CardDeck deck = new CardDeck(cardlist);
+
+            ShowCards(deck.Cards);
+            deck.Shuffle();
+
+            Console.WriteLine();
+            ShowCards(deck.Cards);
+        }
+        public static void ShowCards(List<Card> cards)
+        { 
+            foreach (Card item in cards)
+            {
+                Console.WriteLine(item.Value + " of " + item.Suit);
+            }
+        }
+    }
+}
+
