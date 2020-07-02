e75ad54190cacd575802ad5c5399405344d96612
atte <p2946@student.jamk.fi>
2020-07-02 20:02:32 +0300

    25 tehty

---
+++ T25-Jono/KassaJono.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T25_Jono
+{
+    class KassaJono
+    {
+        public Queue<string> CashierQueue { get; set;}
+        public int Lenght { get; set; }
+        
+        public KassaJono(Queue<string> cashierQueue)
+        {
+            CashierQueue = cashierQueue;
+        }
+        public void GoToLine(string name)
+        {
+            CashierQueue.Enqueue(name);
+        }
+        public string LeaveLine()
+        {
+            return "Palvellaan asiakasta: " + CashierQueue.Dequeue();
+        }
+        public int CheckLenght()
+        {
+            Lenght = CashierQueue.Count;
+            return Lenght;
+        } 
+    }
+}
---
+++ T25-Jono/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace T25_Jono
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KassaJono queue = new KassaJono(new Queue<string>());
+            while (true)
+            {
+                Console.WriteLine("Anna jonoon tulevan asiakkaan nimi," +
+                    " käsittele asiakas painamalla p");
+
+                string nimi = Console.ReadLine();
+                if (nimi != "" && nimi != "p")
+                {
+                    queue.GoToLine(nimi);
+                }
+                else if(nimi == "p")
+                {
+                    Console.WriteLine(queue.LeaveLine());
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Kauppa kiinni, menkää kotionne");
+                    break;
+                }
+                
+                Console.WriteLine("Asiakkaita jonossa: " + queue.CheckLenght());
+                CheckLine(queue.CashierQueue);
+                Console.WriteLine();
+            }
+
+            static void CheckLine(Queue<string> cashierQueue)
+            {
+                foreach (string name in cashierQueue)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
