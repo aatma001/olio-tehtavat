89e9d11a9041e257ba85fc13c83d6c4cf2977015
atte <p2946@student.jamk.fi>
2020-07-28 18:30:54 +0300

    27 tehty

---
+++ T27-Kulkuneuvot/Kulkuneuvo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T27_Kulkuneuvot
+{
+    class Kulkuneuvo
+    {
+
+        public string Valmistaja { get; set; }
+        public string Malli { get; set; }
+        public int RenkaidenLkm { get; set; }
+        
+        public List<Rengas> Renkaat = new List<Rengas>();
+
+        public Kulkuneuvo(string valmistaja, string malli, int renkaidenLkm)
+        {
+            Valmistaja = valmistaja;
+            Malli = malli;
+            RenkaidenLkm = renkaidenLkm;
+        }
+
+        public void AddTyre(Rengas rengas)
+        {
+            for (int i = 0; i < RenkaidenLkm; i++)
+
+            {
+                Renkaat.Add(rengas);
+            }   
+        }
+    }
+}
---
+++ T27-Kulkuneuvot/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace T27_Kulkuneuvot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kulkuneuvo audi = new Kulkuneuvo("Audi", "A3", 4);
+            Kulkuneuvo honda = new Kulkuneuvo("honda", "Civic", 4);
+            Kulkuneuvo pv = new Kulkuneuvo("Suzuki", "Pv", 2);
+
+            Rengas rengas1 = new Rengas("Hancook", "Pirate", "180/55R16");
+            Rengas rengas2 = new Rengas("Michelin", "Pilot", "200/60R18");
+            Rengas rengas3 = new Rengas("Heidenau", "Sport", "13/3,5");
+
+            audi.AddTyre(rengas1);
+            honda.AddTyre(rengas2);
+            pv.AddTyre(rengas3);
+
+            ShowTyresAdded(audi);
+            PrintTyres(audi);
+
+            ShowTyresAdded(honda);
+            PrintTyres(honda);
+
+            ShowTyresAdded(pv);
+            PrintTyres(pv);
+
+        }
+
+        static void PrintTyres(Kulkuneuvo kulkuneuvo)
+        {
+            Console.WriteLine("Vehicle Name: " + kulkuneuvo.Valmistaja
+                    + " " + "Model: " + kulkuneuvo.Malli + "\nTyres:");
+
+            foreach (Rengas item in kulkuneuvo.Renkaat)
+            {
+                Console.WriteLine(item.Valmistaja + " " + item.Malli
+                    + " " + item.Koko);
+            }
+            Console.WriteLine();
+        }
+
+        static void ShowTyresAdded(Kulkuneuvo kulkuneuvo)
+        {
+            Console.WriteLine("Created a new vehicle " + kulkuneuvo.Valmistaja
+                    + " " + kulkuneuvo.Malli);
+
+            foreach(Rengas item in kulkuneuvo.Renkaat)
+            {
+                Console.WriteLine("Tyre " + item.Valmistaja + " added to "
+                    + kulkuneuvo.Valmistaja);
+            }
+            Console.WriteLine();
+        }
+        
+    }
+}
