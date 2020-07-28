537bc849ad97bec38a535bccf094e058cfd3b57d
atte <p2946@student.jamk.fi>
2020-07-28 18:53:33 +0300

    28 valmis

---
+++ T28-Liiga/Joukkue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T28_Liiga
+{
+    class Joukkue
+    {
+        public string Nimi { get; set; }
+        public string Kotikaupunki { get; set; }
+        public List<Pelaaja> Pelaajat { get; set; }
+
+        public Joukkue(string nimi)
+        {
+            Nimi = nimi;
+            Pelaajat = CreatePlayers(nimi);
+        }
+
+        private List<Pelaaja> CreatePlayers(string nimi) 
+        {
+            List<Pelaaja> pelaajat = new List<Pelaaja>();
+
+            if (nimi == "Kalpa")
+            {
+                pelaajat.Add(new Pelaaja("Kalle", "Hellgren", "Hyökkääjä", 7));
+                pelaajat.Add(new Pelaaja("Alexander", "Ovetskin", "Hyökkääjä", 10));
+                pelaajat.Add(new Pelaaja("Steven", "Häkkinen", "Puolustaja", 66));
+            }
+            else if (nimi == "Jokerit")
+            {
+                pelaajat.Add(new Pelaaja("Teemu", "Selänne", "Puolustaja", 8));
+                pelaajat.Add(new Pelaaja("Matti", "Nykänen", "Hyökkääjä", 5));
+            }
+            return pelaajat;
+        }
+
+        public string ToString()
+        {
+            return "Joukkue: " + Nimi + " Kotikaupunni: " + Kotikaupunki
+                + "\n";
+        }
+    }
+}
---
+++ T28-Liiga/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+
+namespace T28_Liiga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Joukkue joukkue = new Joukkue("Kalpa");
+            joukkue.Kotikaupunki = "Kuopio";
+
+            Console.WriteLine(joukkue.ToString());
+            Console.WriteLine("Pelaajat:");
+            PrintPlayers(joukkue);
+        }
+
+        static void PrintPlayers(Joukkue joukkue)
+        {
+            foreach(Pelaaja item in joukkue.Pelaajat)
+            {
+                Console.WriteLine(item.EtuNimi + " " + item.SukuNimi + " "
+                    + item.PeliPaikka + " Pelinumero: " + item.PeliNumero);
+            }
+        }
+    }
+}
