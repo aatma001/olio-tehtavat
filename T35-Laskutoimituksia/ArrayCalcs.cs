a368c62b2816136b15c8a5dbf49ee55d7cda7758
atte <p2946@student.jamk.fi>
2020-08-19 10:38:50 +0300

    35 valmis

---
+++ T35-Laskutoimituksia/ArrayCalcs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T35_Laskutoimituksia
+{
+    public class ArrayCalcs
+    {
+        public static double CalculateSum(double[] numbers)
+        {
+            double value = 0.0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                value += numbers[i];
+            }
+            return Math.Round(value, 2);
+        }
+
+        public static double CalculateAverage(double[] numbers)
+        {
+            double value = 0.0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                value += numbers[i];
+            }
+            return Math.Round(value / numbers.Length, 2);
+        }
+
+        public static double ShowMin(double[] numbers)
+        {
+            return numbers.Min();
+        }
+
+        public static double ShowMax(double[] numbers)
+        {
+            return numbers.Max();
+        }
+    }
+}
+
---
+++ T35-Laskutoimituksia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace T35_Laskutoimituksia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double[] numbers = { 1.0, 2.0, 3.3, 5.5, 6.3, -4.5, 12.0 };
+
+            Console.WriteLine("Sum: " + ArrayCalcs.CalculateSum(numbers));
+            Console.WriteLine("Average: " +ArrayCalcs.CalculateAverage(numbers));
+            Console.WriteLine("Min: " + ArrayCalcs.ShowMin(numbers));
+            Console.WriteLine("Max: " + ArrayCalcs.ShowMax(numbers));
+ 
+        }
+    }
+}
